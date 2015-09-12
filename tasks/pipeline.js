aeaaf9f8b78b7d8b25203a7ef5fc5356855c8c5c
AlchemicalChest <zhu.ziang.1990@gmail.com>
2015-09-12 03:38:05 -0500

    Correct setup

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -22,11 +22,13 @@ var cssFilesToInject = [
 // Client-side javascript files to inject in order
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-  
+
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
+  'js/jquery-1.11.3.min.js',
+  'js/angular.js',
   'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
