6a827dc9c9a92abab3261d9a276705018ecb4614
AlchemicalChest <zhu.ziang.1990@gmail.com>
2015-09-12 15:00:22 -0500

    Correct setup

--- api/models/User.js
+++ api/models/User.js
@@ -13,6 +13,35 @@ module.exports = {
     firstName: {
       type: 'string',
       required: true
+    },
+    lastName: {
+      type: 'string',
+      required: true
+    },
+    dateOfBirth: {
+      type: 'date',
+      required: true
+    },
+    drivingLicenseNumber: {
+      type: 'string',
+      unique: true
+    },
+    address: {
+      type: 'string',
+    },
+    city: {
+      type: 'string',
+    },
+    state: {
+      type: 'string',
+    },
+    admin: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    citations:{
+      collection: 'citation',
+      via: 'defendant'
     }
   }
 };
