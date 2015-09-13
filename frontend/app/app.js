a396843ab2f8f9b8b0d47ca140058b98b6d8a118
TonyDong <yidong@wustl.edu>
2015-09-12 21:07:08 -0500

    Skeleton works (hopefully)

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -15,9 +15,29 @@ var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
   $http.get("http://ec2-52-3-113-7.compute-1.amazonaws.com:1337/user/1")
     .then(function (response) {
-      $scope.lastnames = response.lastName;
+      //window.alert(response);
+      //$scope.lastnames = response.lastName;
     },
   function(response) {
-    $scope.lastnames = "N/A";
+    //window.alert(JSON.stringify(response));
+    //$scope.lastnames = "N/A";
   });
+});
+
+app.controller('detailViewCtrl', function($scope, $http) {
+  $http.get("http://ec2-52-3-113-7.compute-1.amazonaws.com:1337/citation?citationNumber=137757292")
+    .then(function (response) {
+      $scope.violations = response[0].violations;
+      $scope.defendant = response[0].defendant;
+      $scope.citationDate = response[0].citationDate;
+      $scope.courtLocation = response[0].court_location;
+      $scope.courtAddress = response[0].court_address;
+
+      //window.alert(response);
+      //$scope.lastnames = response.lastName;
+    },
+    function(response) {
+      //window.alert(JSON.stringify(response));
+      //$scope.lastnames = "N/A";
+    });
 });
