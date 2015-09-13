063a327d2b800379dbb35d09bff3d5a0a8a8745f
AlchemicalChest <zhu.ziang.1990@gmail.com>
2015-09-12 23:38:54 -0500

    sessioncontroller

---
+++ backend/api/controllers/SessionController.js
@@ -0,0 +1,40 @@
+/**
+ * SessionController
+ *
+ * @description :: Server-side logic for managing sessions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+    'create': function(req, res, next) {
+        if (req.param('email') && req.param('password')) {
+            User.findOne({email: req.param('email')}, function userFound(err, user) {
+                if (err) next(err);
+                if (user) {
+                    console.log('get that');
+                    require("bcrypt").compare(req.param('password'), user.encryptedPassword, function compareFinished(err, valid) {
+                        if (err) next(err);
+                        if (valid) {
+                            req.session.authenticated = true;
+                            req.session.user = user;
+                            console.log('Successful');
+                            res.ok();
+                        }
+                    });
+                } else {
+                    console.log('No user was found');
+                }
+            });
+        }
+        console.log('Failed');
+        res.forbidden();
+    },
+
+    'destroy': function(req, res, next) {
+        req.session.destroy();
+        res.ok();
+    }
+	
+};
+
